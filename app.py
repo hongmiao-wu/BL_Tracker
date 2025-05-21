4feac6ff0f38579266fd20635b20ecb29ef93296
Wu, Hongmiao <117808968+hongmiao-wu@users.noreply.github.com>
2025-05-21 12:08:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,7 @@ def upload():
     return "未接收到文件"
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
